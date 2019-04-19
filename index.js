6f091b89d4607fc5cd2c0d8b266d40439229f168
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-04-20 00:59:09 +0200

    closes #7

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = function(bundler) {
-  bundler.addAssetType('tag', require.resolve('./RiotAsset'))
+  bundler.addAssetType('riot', require.resolve('./RiotAsset'))
 }
---
+++ test/main.js
@@ -0,0 +1,5 @@
+import MyComponent from './my-component.riot'
+
+console.log(MyComponent)
+
+export default MyComponent
